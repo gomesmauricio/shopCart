dfcad215a4b585aae8102376b499cadaf4c26ee4
unknown <josemauricio.gomes@gmail.com>
2021-08-05 18:04:51 -0300

    Home e carrinho de compras

---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,139 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+import { toast } from 'react-toastify';
+import { api } from '../services/api';
+import { Product, Stock } from '../types';
+
+interface CartProviderProps {
+  children: ReactNode;
+}
+
+interface UpdateProductAmount {
+  productId: number;
+  amount: number;
+}
+
+interface CartContextData {
+  cart: Product[];
+  addProduct: (productId: number) => Promise<void>;
+  removeProduct: (productId: number) => void;
+  updateProductAmount: ({ productId, amount }: UpdateProductAmount) => void;
+}
+
+const CartContext = createContext<CartContextData>({} as CartContextData);
+
+export function CartProvider({ children }: CartProviderProps): JSX.Element {
+  const [cart, setCart] = useState<Product[]>(() => {
+    const storagedCart = localStorage.getItem('@RoketShoes:cart');
+
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
+
+    return [];
+  });
+  //Adicionando produtos ao cart
+  const addProduct = async (productId: number) => {
+    try {
+      const updatedCart = [...cart];
+      const productExists = updatedCart.find(product => product.id === productId);
+
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount = stock.data.amount;
+      const currentAmount = productExists ? productExists.amount : 0;
+      const amount = currentAmount + 1;
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+      if (productExists) {
+        productExists.amount = amount;
+      } else {
+        const product = await api.get(`/products/${productId}`);
+
+        const newPorduct = {
+          ...product.data,
+          amount: 1
+        }
+        updatedCart.push(newPorduct)
+      }
+
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+    } catch {
+      toast.error('Erro na adição do produto')
+    }
+  };
+  //Removendo produto do carrinho
+  const removeProduct = (productId: number) => {
+    try {
+      const updatedCart = [...cart];
+      //com o findindex poderemos utilizar o splice para poder remocer do array
+      const productIndex = updatedCart.findIndex(product => product.id === productId)
+
+      if (productIndex >= 0) {
+        /*O splice pode remover itens do array e, se necessário, adicionar novos itens 
+        no array, preciso passar, apartir de onde quero começar a deletar e a quantidade de 
+        tens a ser deletados
+        */
+        updatedCart.splice(productIndex, 1);
+        setCart(updatedCart);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+      } else {
+        throw Error();
+      }
+    } catch {
+      toast.error('Erro na remoção do produto');
+    }
+  };
+
+  /*Esse metodo updateProductAmount perpetua o valor no carrinho, sai 
+  instantaneamente da funcção se a quantidade do produto foir menor 
+  ou igual a zero,
+  */
+  const updateProductAmount = async ({
+    productId,
+    amount,
+  }: UpdateProductAmount) => {
+    try {
+     if(amount <= 0){
+       return;
+     }
+//Verificação do estoque
+     const stock = await api.get(`/stock/${productId}`);
+
+     const stockAmount = stock.data.amount;
+     if(amount > stockAmount){
+       toast.error('Quantidade solicitada fora do estoque');
+       return;
+     }
+       const updatedCart = [...cart];
+       const productExists = updatedCart.find(product => product.id === productId);
+       if(productExists){
+         productExists.amount = amount;
+         setCart(updatedCart);
+         localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+       }else{
+         throw Error();
+       }
+     
+    } catch {
+      toast.error('Erro na alteração de quantidade do produto')
+    }
+  };
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addProduct, removeProduct, updateProductAmount }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart(): CartContextData {
+  const context = useContext(CartContext);
+
+  return context;
+}
